db05e41b01bb37498bdb33a397df091023b3bdb8
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-30 09:44:07 +0300

    added gitignore file

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='1234567890'
 export MAIL_USERNAME='davidmaina085@gmail.com'
-export MAIL_PASSWORD='41506133'
-# export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kiprotich@localhost/postgres'
+# export MAIL_PASSWORD='41506133'
+
 
 python3.8 manage.py server
